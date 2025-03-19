a0412123ab1de8951a9f69c06e1fe3f151af5ade
admin <admin@192.168.0.146>
2025-03-19 14:12:45 +0800

    web

---
+++ springboot-websocket/src/main/java/com/example/websocket/channel/ChatChannel.java
@@ -0,0 +1,73 @@
+package com.example.websocket.channel;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.CloseReason;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+/**
+ * @ServerEndpoint 表示一個 WebSocket 端點
+ * "/channel/chat" 表示連接路徑: ws://localhost:8080/channel/chat
+ * */
+
+@ServerEndpoint(value = "/channel/chat")
+public class ChatChannel {
+	// 利用 Set + synchronized 來保存所有的 sessin 物件
+	private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
+	
+	// 廣播發送
+	public void broadcast(String message) {
+		// 逐筆發送
+		sessions.forEach((session) -> {
+			if(session.isOpen()) {
+				session.getAsyncRemote().sendText(message);
+			}
+		});
+	}
+	
+	// 當客戶端與伺服端建立連接時會自動觸發
+	@OnOpen
+	public void onOpen(Session session) {
+		// session id: 每一個連線都有獨立的 session id 值, 以便區隔來源
+		System.out.println("有人連進來了 session id = " + session.getId());
+		// 將 session 保存在 sessions 中
+		sessions.add(session);
+		broadcast("有人上線了 id: " + session.getId());
+	}
+	
+	// 當客戶端傳送字串資料到伺服端時觸發
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		System.out.println("session id = " + session.getId() + " 說(message): " + message);
+		// 單筆發送
+		//session.getAsyncRemote().sendText(message);
+		// 進行廣播發送
+		broadcast("[" + session.getId() + "] 說: " + message);
+	}
+	
+	// 當客戶端與伺服端關閉連接時會自動觸發
+	@OnClose
+	public void onClose(Session session, CloseReason closeReason) {
+		System.out.println("session id = " + session.getId() + " 已關閉連線, 原因: " + closeReason);
+		// 將該 session 從 sessions 中移除
+		sessions.remove(session);
+		broadcast("有人離線了 id: " + session.getId());
+	}
+	
+	// 當客戶端與伺服端通訊過程中發生錯誤時會自動觸發
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		System.out.println("session id = " + session.getId() + " 發生連線錯誤, 原因: " + throwable);
+	}
+	
+	
+	
+}
---
+++ springboot-websocket/src/main/resources/static/js/chat.js
@@ -0,0 +1,57 @@
+var webSocket;
+const url = 'ws://localhost:8080/channel/chat';
+const connectButton = document.getElementById('connectButton');
+const closeButton = document.getElementById('closeButton');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+const log = document.getElementById('log');
+
+// 更新對話紀錄
+const addLog = (message) => {
+	log.textContent += `${message}\n`;
+	//log.textContent += (message + '\n');
+}
+
+// 連接按鈕
+connectButton.onclick = () => {
+	webSocket = new WebSocket(url);
+	
+	webSocket.onopen = () => {
+		addLog('WebSocket 連接成功');
+		connectButton.disabled = true;
+		closeButton.disabled = false;
+		messageInput.disabled = false;
+		sendButton.disabled = false;
+	};
+	
+	webSocket.onmessage = (event) => {
+		addLog("收到消息: " + event.data);	
+	};
+	
+	webSocket.onclose = (event) => {
+		addLog('WebSocket 離線成功');
+		connectButton.disabled = false;
+		closeButton.disabled = true;
+		messageInput.disabled = true;
+		sendButton.disabled = true;	
+	};
+	
+	webSocket.onerror = (event) => {
+			
+	};			
+};
+
+
+// 廣播-發送按鈕
+sendButton.onclick = () => {
+	const message = messageInput.value;
+	webSocket.send(message);
+};
+
+// 斷開按鈕
+closeButton.onclick = () => {
+	if(webSocket) { // 是否在 open 狀態
+		webSocket.close();
+	}
+};
+
